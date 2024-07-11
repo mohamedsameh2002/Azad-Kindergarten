3d7da301934993b059aaf34c80ddb38aae2e383f
Mohamed sameh <mohamed.403.sameh@gmail.com>
2024-07-11 15:50:30 +0300

    test

--- forms/urls.py
+++ forms/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = "competition"
+app_name = "forms"
 
 urlpatterns = [
 path("register/", views.registration_in_school,name='register'),
